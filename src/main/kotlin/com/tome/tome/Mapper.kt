c34c31ead3719bbd16cb59956bc11f2d78ecdb20
Eliseo <ealanistoledo@mail.austral.edu.ar>
2025-08-26 09:56:01 -0300

    added toDataDTO function in mapper

--- src/main/kotlin/com/tome/tome/Mapper.kt
+++ src/main/kotlin/com/tome/tome/Mapper.kt
@@ -1,9 +1,23 @@
 package com.tome.tome
 
 import com.tome.tome.book.Book
-import com.tome.tome.bookDataDTO.BookDTO
+import com.tome.tome.book.dto.BookDTO
 
 fun toDTO(book: Book): BookDTO = BookDTO(
     id = book.id,
     title = book.title,
+)
+
+fun toDataDTO(book: Book) = com.tome.tome.book.dto.BookDataDTO(
+    id = book.id,
+    title = book.title,
+    coverUrl = book.coverUrl,
+    synopsis = book.synopsis,
+    isbn = book.isbn,
+    year = book.year,
+    authors = book.authors.map { it.name },
+    publishers = book.publishers.map { it.name },
+    languages = book.languages.map { it.name },
+    pages = book.pages,
+    tags = book.tags.map { it.name }
 )
